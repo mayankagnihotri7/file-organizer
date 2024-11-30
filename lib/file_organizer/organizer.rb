8863285bed65909f02f1a950027b7d25622bad35
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2024-11-30 22:03:40 +0530

    add logic to organize method

--- lib/file_organizer/organizer.rb
+++ lib/file_organizer/organizer.rb
@@ -2,9 +2,9 @@
 
 module FileOrganizer
   class Organizer
-    attr_reader :directory, :types, :new_folder
+    attr_reader :directory, :types, :new_folder, :new_path
 
-    def initialize(directory, new_folder: false)
+    def initialize(directory, new_folder: false, new_path: nil)
       @directory = directory
       @types = {
         images: ['.jpg', '.jpeg', '.png', '.gif'],
@@ -14,10 +14,37 @@ module FileOrganizer
         archives: ['.zip', '.tar', '.rar', '.gz']
       }
       @new_folder = new_folder
+      @new_path = new_path
     end
 
     def organize
-      # organizing logic goes here
+      Dir.foreach do |file|
+        file_ext = File.extname(file)
+        category = types.find { |_key, exts| exts.include?(file_ext) }
+
+        folder_name = get_folder_name(category)
+
+        destination = if new_folder && !new_path.empty?
+                        Dir.exist?(new_path) ? new_path : FileUtils.mkdir_p(folder_name)
+                      else
+                        new_path.empty? ? folder_name : new_path
+                      end
+
+        FileUtils.mv(file, destination)
+      end
+    end
+
+    private
+
+    def get_folder_name(category)
+      case category
+      when :images then 'Pictures'
+      when :documents then 'Documents'
+      when :audio then 'Music'
+      when :videos then 'Videos'
+      else
+        'Archives'
+      end
     end
   end
 end
